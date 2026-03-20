9084cb48d12d8f1d59d30a1f20f2a24936d7e3e6
YairVera <yairvera44@gmail.com>
2026-03-20 14:36:40 -0500

    while loops

---
+++ while-loops.js
@@ -0,0 +1,9 @@
+// Array of Complex Objects
+import employees from './data.json' with { type: 'json'}
+
+//Basic while loop
+let i = 0;
+while(employees[i]) {
+    console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
+    i++;
+}
