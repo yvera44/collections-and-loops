0cc68b6a2baedba42d429480eb18b65a265addc9
YairVera <yairvera44@gmail.com>
2026-03-20 14:41:31 -0500

    do-while loop

--- while-loops.js
+++ while-loops.js
@@ -7,3 +7,12 @@ while(employees[i]) {
     console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
     i++;
 }
+
+console.log ('------');
+
+//Basic do-while loop
+i = 0;
+do{
+    console.log(`Name: ${employees[i].firstName} ${employees[i].lastName}`);
+    i++;
+} while(employees[i]);
