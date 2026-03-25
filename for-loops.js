954be687323ce36cf9f641edc7540ec9aa8b9199
YairVera <yairvera44@gmail.com>
2026-03-25 18:04:50 -0500

    for-loops

---
+++ for-loops.js
@@ -0,0 +1,32 @@
+// Array of Data 
+const names = ["Yair", "Esmeralda", "Steve", "Cartman"];
+
+// Quitck Example of a for loop
+for (let i = 0; i < names.length; i++) {
+    console.log(`Name: ${names[i]}`);
+}
+
+// Using a for... of loop
+for (let name of names) {
+    console.log(`Name: ${name}`);
+}
+
+//Array of Complex Objects
+import employees from './data.json' with { type: 'json'}
+
+const employee = employees[0];
+for (let property in employee) {
+    console.log(`${property}: ${employee[property]}`);
+}
+
+console.log(`
+    -----------------------`
+);
+
+//Bringing both approaches together 
+for (let emp of employees) {
+    for (let property in emp){
+        console.log(`${property}: ${emp[property]}`);
+    }
+    console.log('--');
+}
